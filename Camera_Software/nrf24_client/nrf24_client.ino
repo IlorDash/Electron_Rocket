24936d1e431b81a593e49c6910feefb20bf67991
Ilor5 <48730197+Ilor5@users.noreply.github.com>
2020-07-30 00:40:05 +0300

    added code for ESP32 Cam with nrf24 and radio library TODO: check program

---
+++ Camera_Software/nrf24_client/nrf24_client.ino
@@ -0,0 +1,20 @@
+#include  <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+char msg[6] = "hello";
+RF24 radio(2, 15, 14, 12, 13);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+void setup(void) {
+  Serial.begin(115200);
+  radio.begin();
+  radio.setChannel(2);
+  radio.setPayloadSize(7);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(pipe);
+}
+void loop(void) {
+  Serial.println("send ...");
+  radio.write(msg, 6);
+  delay(3000);
+}
